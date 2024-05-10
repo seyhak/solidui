9fb85d2b1dba6a28124461b7fffe4fa8de6fd37b
Seyhak <seyhakly@gmail.com>
2024-05-10 09:06:57 +0200

    add application and + buttons

--- src/pages/Applications/Application/Application.tsx
+++ src/pages/Applications/Application/Application.tsx
@@ -1,4 +1,21 @@
+import { useParams } from "@solidjs/router"
+import { For } from "solid-js"
+import { applicationFactory } from "src/utils/factories"
+
 export const Application = () => {
-  return 1
+  const params = useParams()
+  const application = applicationFactory()
+
+  return (
+    <div>
+      <h1>{params.name}</h1>
+      <ol>
+        <For each={application.users}>
+          {(user) => <li>{`${user.position} - ${user.name}`}</li>}
+        </For>
+      </ol>
+      <h1>{application.isActive ? "✔️" : "⭕"}</h1>
+    </div>
+  )
 }
 export default Application
