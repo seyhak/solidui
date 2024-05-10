f755a6e7c8070a485b444b816fab3e8fd0dc39fc
Seyhak <seyhakly@gmail.com>
2024-05-10 15:27:46 +0200

    chart

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,3 +1,31 @@
-export const Chart = () => {
-  return <div></div>
+import { Application } from "src/types/types"
+import { Show, createEffect, createMemo, createSignal } from "solid-js"
+import { LinePlot } from "./LineChart"
+
+type ChartProps = {
+  data?: Application[]
+}
+
+export const Chart = (props: ChartProps) => {
+  const data = () => props.data
+  console.log({ data: props.data })
+  const [ys, setYs] = createSignal<number[]>([])
+  const dataValues = createMemo(() => {
+    console.log("memo")
+    return props.data?.map((x) => x.price)
+  })
+  createEffect(() => {
+    console.log("Create effect", data())
+    console.log("Create effect dataValues", dataValues()?.length)
+    const neww = data()?.map((x) => x.price)
+    console.log("neww", neww?.length)
+    setYs(neww || [])
+  })
+  // })
+  return (
+    <>
+      <LinePlot data={dataValues()} />
+      {/* <LinePlot data={ys()}/> */}
+    </>
+  )
 }
